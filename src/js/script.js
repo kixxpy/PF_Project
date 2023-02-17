bd95617e1dc55e3b7df6b609f161220b95b8104a
Kirill Eliseev <ekn62@bk.ru>
2023-02-17 17:47:15 +0300

    skills ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
+const overlay = document.querySelector('.menu__overlay');
+const counter = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 close.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+counter.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
